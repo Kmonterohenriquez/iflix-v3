50b7f857409b42b46baa9fe886d1fbcc38ae3d9b
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-13 01:49:04 -0400

    Popular movies view created and done

---
+++ src/views/movie/PopularMovies.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Loader from 'components/hoc/Loader'; 
+import MovieList from 'components/movies/MovieList';
+import Container from 'components/common/Container';
+import PaginationBar from 'components/common/PaginationBar';
+import { fetchPopularMovies } from 'actions/movieActions';
+import { isEmpty, numberWithCommas } from 'helpers/helperFunctions';
+
+const PopularMovies = (props) => {
+  const { popularMovies, isLoading, favorites } = useSelector(state => ({
+    popularMovies: state._movies.popularMovies,
+    isLoading: state._misc.isLoading,
+    favorites: state._misc.favorites
+  }));
+  const dispatch = useDispatch();
+  const route = '/movie/popular';
+  
+  useEffect(() => {
+    if (isEmpty(popularMovies)) {
+      dispatch(fetchPopularMovies(route));
+    }
+  }, []);
+
+  const handlePageChange = (page) => {
+    if (popularMovies.page !== page && !isLoading) {
+      dispatch(fetchPopularMovies(route, page));
+    }
+  };
+
+  return (
+    <Container>
+      <div className="movie__header">
+        <div className="movie__header-title">
+          <h1>Popular Movies</h1>
+          <h3>{numberWithCommas(popularMovies.total_results)} Movies</h3>
+        </div>
+      </div>
+      <MovieList 
+          movies={popularMovies.results} 
+          favorites={favorites}
+          templateCount={10} 
+      />
+      <PaginationBar 
+          activePage={popularMovies.page}
+          itemsCountPerPage={1}
+          onChange={handlePageChange}
+          pageRangeDisplayed={10}
+          totalItemsCount={popularMovies.total_pages}
+          totalPage={popularMovies.total_pages}
+      />
+    </Container>  
+  );
+};
+
+PopularMovies.propTypes = {
+  fetchPopularMovies: PropTypes.func,
+  popularMovies: PropTypes.shape({
+    page: PropTypes.number,
+    total_page: PropTypes.number,
+    total_results: PropTypes.number,
+    results: PropTypes.arrayOf(PropTypes.object)
+  })
+};
+
+export default Loader('popularMovies')(PopularMovies);
