e6400e8361a1b0a74dd4b8ce6e16e0012daab16e
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-13 02:00:42 -0400

    Casting View created

---
+++ src/views/people/ViewCasting.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import imgBackground from 'images/background.jpg';
+import MovieList from 'components/movies/MovieList';
+
+import { isEmpty } from 'helpers/helperFunctions';
+
+const ViewPictures = ({ history }) => {
+  const { actor, casting, favorites } = useSelector(state => ({
+    actor: state._people.person.actor,
+    casting: state._people.person.casting,
+    favorites: state._misc.favorites
+  }));
+
+  useEffect(() => {
+    if (isEmpty(actor)) {
+      history.goBack();
+    }
+  }, []);
+
+  return !isEmpty(actor) && (
+    <>
+      <div className="posters__banner">
+        <img src={imgBackground} alt=""/>
+        <div className="posters__banner-content">
+          <h1>{actor.name}</h1>
+          <button 
+              className="button--back"
+              onClick={history.goBack}>
+            Back
+          </button>
+        </div>
+      </div>
+      <div className="container__wrapper">
+        <div className="movie__header">
+          <div className="movie__header-title">
+            <h1>Casted Movies</h1>
+            <h3>{casting.length} Movies</h3>
+          </div>
+        </div>  
+        <MovieList
+            category="movie"
+            favorites={favorites}
+            movies={casting} 
+        />
+      </div>
+    </>
+  );
+};
+
+export default ViewPictures;
