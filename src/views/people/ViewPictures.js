097c4e1ee56c0d7f1482f255456966f81780c087
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-13 02:01:26 -0400

    Pictures View created

---
+++ src/views/people/ViewPictures.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import PersonProfiles from 'components/people/PersonProfiles';
+import imgBackground from 'images/background.jpg';
+
+import { isEmpty } from 'helpers/helperFunctions';
+
+const ViewPictures = ({ history }) => {
+  const actor = useSelector(state => state._people.person.actor);
+
+  useEffect(() => {
+    if (isEmpty(actor)) {
+      history.goBack();
+    }
+  }, []);
+
+  return !isEmpty(actor) && (
+    <>
+      <div className="posters__banner">
+        <img src={imgBackground} alt=""/>
+        <div className="posters__banner-content">
+          <h1>{actor.name}</h1>
+          <button 
+              className="button--back"
+              onClick={history.goBack}>
+            Back
+          </button>
+        </div>
+      </div>
+      <div className="container__wrapper">
+        <PersonProfiles
+            id={actor.id}
+            posters={actor.images.profiles} 
+        />
+      </div>
+    </>
+  );
+};
+
+export default ViewPictures;
