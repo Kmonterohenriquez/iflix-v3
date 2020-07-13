b80420c22425905ffe5e9e3334cf8ac36908a78e
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-13 13:32:18 -0400

    Search Tv Tab view done

---
+++ src/views/search/tab/SearchTvTab.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import MovieList from 'components/movies/MovieList';
+import PaginationBar from 'components/common/PaginationBar';
+
+// actions
+import { searchTvShows } from 'actions/searchActions';
+
+// helpers
+import { isEmpty } from 'helpers/helperFunctions';
+
+const SearchTvTab = ({ tvShows, favorites, isLoading, query }) => {
+  const dispatch = useDispatch();
+
+  const handlePageChange = (page) => {
+    if (tvShows.page !== page && !isLoading) {
+      dispatch(searchTvShows(`/search/tv?query=${query}`, page));
+    }
+  };
+
+  return !isEmpty(tvShows) && tvShows.results.length !== 0 ? (
+    <>
+      <MovieList 
+          category="tv"
+          movies={tvShows.results}
+          favorites={favorites}
+      />
+      <PaginationBar 
+          activePage={tvShows.page}
+          itemsCountPerPage={1}
+          onChange={handlePageChange}
+          pageRangeDisplayed={10}
+          totalItemsCount={tvShows.total_pages}
+          totalPage={tvShows.total_pages}
+      />
+    </>
+  ) : (
+    <div className="search__no-result">
+      <h1>No TV show found.</h1>
+      <i className="fa fa-theater-masks" />
+    </div>
+  );
+};
+
+export default SearchTvTab;
