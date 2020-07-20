5a47a692f53cb2db57b5f2695736225749871645
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-20 10:40:49 -0400

    mserver setup done

---
+++ Server/Index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/../build`));
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
