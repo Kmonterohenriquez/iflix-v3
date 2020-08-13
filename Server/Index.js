45f5541f336b62d53e47c40068c916e8086663b8
Kevin Montero <kevinwilding1997@gmail.com>
2020-08-13 15:20:05 -0400

    new server port

--- Server/Index.js
+++ Server/Index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/../build`));
 
-const port = 5000;
+const port = 4005;
 app.listen(port, () => console.log(`Server running on port ${port}`));
